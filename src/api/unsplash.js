0de682fcfd7244757e7f90cbe0c89de9d09a2dfc
dvankevich <dvankevich@gmail.com>
2025-12-12 22:00:16 +0200

    test api

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,8 +4,6 @@ const apiUrl = "https://api.unsplash.com/search/photos";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export const fetchImages = async (query, page = 1, perPage = 12) => {
-  console.log("API key:", apiKey);
-
   try {
     const response = await axios.get(apiUrl, {
       params: {
