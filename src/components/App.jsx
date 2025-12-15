098493c63b847ba35c3030196b4a4d6705ae0691
dvankevich <dvankevich@gmail.com>
2025-12-15 18:00:01 +0200

    add states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,42 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import SearchBar from "./SearchBar/SearchBar.jsx";
 import { fetchImages } from "../api/unsplash";
 
 function App() {
-  const searchImages = async (query) => {
-    try {
-      const images = await fetchImages(query);
-      return images;
-    } catch (error) {
-      console.error("Failed to retrieve images:", error);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (query === "") {
+      return;
     }
-  };
+    const searchImages = async (query) => {
+      try {
+        const images = await fetchImages(query);
+        return images;
+      } catch (error) {
+        console.error("Failed to retrieve images:", error);
+      }
+    };
+
+    searchImages("grogu").then((images) => {
+      console.log(images);
+      setResults(images);
+    });
+  }, [query, page]);
 
   // for testing purposes
-  searchImages("grogu").then((images) => {
-    console.log(images);
-  });
+  useState(() => {
+    console.log(results);
+  }, [results]);
 
   const handleSearch = (searchTerm) => {
-    console.log("Search submitted with query:", searchTerm);
+    setResults([]);
+    setQuery(searchTerm);
+    setPage(1);
   };
 
   return (
