23713f6a4e83a52ae39d84d6f40fa1016b95265c
dvankevich <dvankevich@gmail.com>
2025-12-17 20:32:36 +0200

    remove try catch from unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,24 +1,18 @@
 import axios from "axios";
-//import mockImages from "./example.json";
 
 const apiUrl = "https://api.unsplash.com/search/photos";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export const fetchImages = async (query, page = 1, perPage = 12) => {
-  try {
-    const response = await axios.get(apiUrl, {
-      params: {
-        query,
-        page,
-        per_page: perPage,
-      },
-      headers: {
-        Authorization: `Client-ID ${apiKey}`,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching images from Unsplash:", error.message);
-    throw error;
-  }
+  const response = await axios.get(apiUrl, {
+    params: {
+      query,
+      page,
+      per_page: perPage,
+    },
+    headers: {
+      Authorization: `Client-ID ${apiKey}`,
+    },
+  });
+  return response.data;
 };
